62e4948dcc5a2550a8c4be363df7c0328f57ee06
Jinyehee <ounnms@gmail.com>
2025-04-18 00:51:08 +0900

    조금 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import main_logo from "./assets/main_logo.png";
 import modeIcon from "./assets/lightmode.png";
 import langIcon from "./assets/kr.png";
 import searchIcon from "./assets/search.png";
+import { useEffect } from "react";
 
 function App() {
+   useEffect(()=>{
+      async function getPokemon() {
+         try {
+            const response = await fetch("https://pokeapi.co/api/v2/pokemon-species/1/");
+            const data = await response.json();
+            console.log(data)
+         } catch (error) {
+            console.log("오류났습니다");
+         }
+         
+      }
+      getPokemon();
+   })
    return (
-      <div className="bg-gray-200 h-screen">
+      <div className="bg-gray-50 h-screen">
          <header className="relative flex items-center p-6  shadow-md shadow-gray-300">
             <div className="absolute left-1/2 transform -translate-x-1/2">
                <img src={main_logo} alt="로고" className="h-20" />
@@ -19,11 +33,11 @@ function App() {
                </button>
             </div>
          </header>
-         <div className="flex justify-center p-10 gap-4">
+         <div className="flex justify-center p-10 gap-2">
             <input
                type="text"
-               placeholder="원하는 포켓몬을 검색하세요!!"
-               className="px-4 py-2  bg-gray-600 text-white focus:outline-none w-[400px]"
+               placeholder="원하는 포켓몬을 검색하세요."
+               className="px-4 py-2 rounded-4xl bg-gray-600 text-white focus:outline-none w-[400px]"
             />
             <button className="p-3 rounded-4xl">
                <img className="w-6" src={searchIcon} alt="" />
